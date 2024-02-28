f5d2f0cf2807d7e7255eb99d684f85f0cb73ebf2
alejosanchez99 <alejosanchezgomez@gmail.com>
2024-02-27 20:40:39 -0500

    adding route layous, layout groups, breaking out of layouts and API Routes.

---
+++ SvelteKit/src/routes/api/comments/[commentId]/+server.js
@@ -0,0 +1,40 @@
+import { json } from "@sveltejs/kit";
+import { comments } from "$lib/comments.js";
+
+export function GET(requestEvent) {
+    const { params } = requestEvent;
+    const { commentId } = params;
+    const comment = findComment(commentId);
+
+    return json(comment);
+}
+
+export async function PATCH(requestEvent) {
+    const { params, request } = requestEvent;
+    const { commentId } = params;
+    const { text } = await request.json();
+
+    const comment = findComment(commentId);
+    comment.text = text;
+
+    return json(comment);
+}
+
+export async function DELETE(requestEvent) {
+    const { params, request } = requestEvent;
+    const { commentId } = params;
+    const deletedComment = findComment(commentId);
+    removeComent(commentId);
+
+    return json(deletedComment);
+}
+
+function removeComent(commentId) {
+    const index = comments.findIndex((comment) => comment.id === parseInt(commentId))
+
+    comments.splice(index, 1);
+}
+
+function findComment(commentId) {
+    return comments.find((comment) => comment.id === parseInt(commentId));
+}
